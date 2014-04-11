f93373ed9bfe9a17548796348995e075c2b3653d
Mark Mandel <mark.mandel@gmail.com>
2014-04-12 09:25:52 +1000

    Fixed the bug with side hits!!! Some cleanup.

--- desktop/src-common/brute_play_pong/physics.clj
+++ desktop/src-common/brute_play_pong/physics.clj
@@ -4,8 +4,7 @@
           [play-clj.math])
     (:require [brute.entity :as e]
               [clojure.math.numeric-tower :as m])
-    (:import [brute_play_pong.component Rectangle Ball Velocity Paddle]
-             [com.badlogic.gdx.math Intersector]))
+    (:import [brute_play_pong.component Rectangle Ball Velocity Paddle]))
 
 (def max-rotate-on-bounce 55)
 
@@ -21,7 +20,8 @@
           screen-width (graphics! :get-width)]
         (> (+ geom-x geom-width) screen-width)))
 
-(defn- keep-rects-in-world
+(defn- keep-rects-in-world!
+    "Make sure all rectangles stay inside the walls"
     []
     (doseq [entity (e/get-all-entities-with-component Rectangle)]
         (let [rect (e/get-component entity Rectangle)
@@ -42,12 +42,10 @@
 (defn- pos-multiplier
     "If n1 is less than n2, return 1, otherwise return -1"
     [n1 n2]
-    (if (< n1 n2)
-        1
-        -1))
+    (if (< n1 n2) 1 -1))
 
-(defn- bounce-ball
-    "Bounce the ball"
+(defn- bounce-ball!
+    "Bounce the ball. Returns :top-bottom if hits the top or bottom :side if hits the side."
     [p-rect b-rect vel]
 
     ; http://gamedev.stackexchange.com/questions/25642/breakout-collision-using-2d-rectangles
@@ -63,46 +61,38 @@
           width (rectangle! intersec :get-width)
           height (rectangle! intersec :get-height)]
 
-        (println "intersec: " intersec)
-        (println "[" didit "] " "w/h: " width " / " height)
-
         ; need to change the velocity, and also move it outside of the paddle, as it
         ; is overlapping, which can cause all sorts of weirdness.
 
         (if (>= width height)
             ; mutable data
-            (do (println "TOP/BOTTOM")
-                (set! (.y vel) (* -1 (vector-2! vel :y)))
+            (do (set! (.y vel) (* -1 (vector-2! vel :y)))
                 (let [y (rectangle! b-rect :get-y)]
-                    (rectangle! b-rect :set-y (-> (* (pos-multiplier (rectangle! p-rect :get-y) y) (rectangle! intersec :get-height)) (+ y)))))
-            (do (println "SIDE")
-                (set! (.x vel) (* -1 (vector-2! vel :x)))
+                    (rectangle! b-rect :set-y (-> (* (pos-multiplier (rectangle! p-rect :get-y) y) (rectangle! intersec :get-height)) (+ y))))
+                :top-bottom)
+            (do (set! (.x vel) (* -1 (vector-2! vel :x)))
                 (let [x (rectangle! b-rect :get-x)]
-                    (rectangle! b-rect :set-x (-> (* (pos-multiplier (rectangle! p-rect :get-x) x) (rectangle! intersec :get-width)) (+ x))))))
-        )
-    )
+                    (rectangle! b-rect :set-x (-> (* (pos-multiplier (rectangle! p-rect :get-x) x) (rectangle! intersec :get-width)) (+ x))))
+                :side))))
 
-(defn- ball-collision
+(defn- ball-collision!
     "Bounces the ball off the rectangle, accounting for the angle of hit"
     [ball-rect paddle vel]
     (let [p-rect (:rect (e/get-component paddle Rectangle))
           ;; positive is right, negative is left
           center-diff (vector-2! (rectangle! ball-rect :get-center (vector-2*)) :sub (rectangle! p-rect :get-center (vector-2*)))
           abs-x (m/abs (vector-2! center-diff :x))
-          rotation (* -1 (/ (vector-2! center-diff :x) 50) max-rotate-on-bounce)]
-        (bounce-ball p-rect ball-rect vel)
-        ;; rotate the velocity depending on where it is
-        (vector-2! vel :rotate rotation)
-        ;when on the out edge, speed the ball up a bit
-        (cond (> abs-x 35) (do (println "FASTER!!!")
-                               (vector-2! vel :scl (float 1.1)))
-              (< abs-x 5) (do (println "SLOWER!")
-                              (vector-2! vel :scl (float 0.95)))
-              )
-        )
-    )
+          rotation (* -1 (/ (vector-2! center-diff :x) 50) max-rotate-on-bounce)
+          bounce-side (bounce-ball! p-rect ball-rect vel)]
 
-(defn- move-ball
+        ;; rotate the velocity depending on where it is, and only for top/bottom collisions
+        (when (= bounce-side :top-bottom)
+            (vector-2! vel :rotate rotation))
+        ;when on the out edge, speed the ball up a bit. When near the center, slow it down a touch.
+        (cond (> abs-x 35) (vector-2! vel :scl (float 1.2))
+              (< abs-x 5) (vector-2! vel :scl (float 0.95)))))
+
+(defn- move-ball!
     "Move the ball based on it's velocity, and if it bounces into anything"
     [delta]
     (doseq [entity (e/get-all-entities-with-component Ball)]
@@ -121,10 +111,10 @@
 
             ;;bouncing off paddles
             (when-let [paddle (touching-paddle rect)]
-                (ball-collision rect paddle vel)))))
+                (ball-collision! rect paddle vel)))))
 
 (defn process-one-game-tick
     "Physics, process one game tick"
     [delta]
-    (move-ball delta)
-    (keep-rects-in-world))
+    (move-ball! delta)
+    (keep-rects-in-world!))
